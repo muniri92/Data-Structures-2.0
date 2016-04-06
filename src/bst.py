cbdd1a7bed2c312ca55665c77c90247cc214777a
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-05 19:19:08 -0700

    left node insert is working and tested.

--- src/bst.py
+++ src/bst.py
@@ -2,16 +2,43 @@
 """Binary Search Tree Module."""
 
 
+class Node(object):
+    """Define a node class."""
+
+    def __init__(self, value, parent=None, left=None, right=None):
+        """Initialize a Node object."""
+        self.value = value
+        self.parent = parent
+        self.left = left
+        self.right = right
+
+
 class BST(object):
     """Define Binart Search Tree class(BST)."""
 
-    def __init__(self, value=[]):
-        """Initialize BST class."""
-        self.heap = []
+    top = None
 
-    def contains(self, value):
-        """Check to see if value is in heap."""
-        for i in self.heap:
-            if i == value:
-                return True
-        return False
+    def __init__(self, values):
+        """Initialize BST class."""
+        if isinstance(values, list):
+            for value in values:
+                self.insert(value)
+        else:
+            raise TypeError("Please package your item into a list!")
+
+    def insert(self, value):
+        """Insert a value into the binary heap."""
+        # if self.contains(value):
+        #     break
+        # else:
+        cursor = self.top
+        new_node = Node(value)
+        if self.top is None:
+            self.top = new_node
+        else:
+            while cursor is not None:
+                if cursor.value > new_node.value:
+                    old_cursor = cursor
+                    cursor = cursor.left
+            new_node.parent = old_cursor
+            old_cursor.left = new_node
--- src/test_bst.py
+++ src/test_bst.py
@@ -3,19 +3,29 @@
 import pytest
 
 
-HEAP_TEST = [3, 2, 4, 1, 5]
+# HEAP_TEST = [3, 2, 4, 1, 5]
 
-CONTAINS_TEST = [
-    (1, True),
-    (7, False),
-    (3, True),
-]
+# CONTAINS_TEST = [
+#     (1, True),
+#     (7, False),
+#     (3, True),
+# ]
 
 
-@pytest.mark.parametrize('value, result', CONTAINS_TEST)
-def test_contains(value, result):
-    """Test contain function in BST."""
+# @pytest.mark.parametrize('value, result', CONTAINS_TEST)
+# def test_contains(value, result):
+#     """Test contain function in BST."""
+#     from bst import BST
+#     b = BST()
+#     b.heap = HEAP_TEST
+#     assert b.contains(value) == result
+
+
+def test_insert_left():
     from bst import BST
-    b = BST()
-    b.heap = HEAP_TEST
-    assert b.contains(value) == result
+    b = BST([20])
+    b.insert(16)
+    b.insert(14)
+    assert b.top.value == 20
+    assert b.top.left.value == 16
+    assert b.top.left.left.value == 14
