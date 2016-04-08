2e1fe2dab43a82cdc22b35519d5fe52bb4502946
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-07 18:42:15 -0700

    Created fixture and split out tests.

--- src/test_bst.py
+++ src/test_bst.py
@@ -3,58 +3,62 @@
 import pytest
 
 
-def test_insert_left():
+@pytest.fixture(scope='function')
+def big_left():
+    """Fixture for testing."""
+    from bst import BST
+    b = BST([20])
+    b.insert(16)
+    b.insert(14)
+    b.insert(17)
+    b.insert(14)
+    b.insert(23)
+    return b
+
+
+def test_insert_top(big_left):
     """Test that insert works."""
-    from bst import BST
-    b = BST([20])
-    b.insert(16)
-    b.insert(14)
-    b.insert(17)
-    b.insert(23)
-    assert b.top.value == 20
-    assert b.top.left.value == 16
-    assert b.top.left.right.value == 17
-    assert b.top.left.left.value == 14
-    assert b.top.right.value == 23
+    assert big_left.top.value == 20
 
 
-def test_contains():
+def test_insert_left(big_left):
+    """Test that insert smaller value goes left."""
+    assert big_left.top.left.value == 16
+
+
+def test_insert_right(big_right):
+    """Test that insert right works correctly."""
+    assert big_left.top.left.right.value == 17
+
+
+def test_contains(big_left):
     """Test contains function."""
-    from bst import BST
-    b = BST([20])
-    b.insert(16)
-    b.insert(14)
-    b.insert(17)
-    b.insert(23)
-    assert b.contains(20) is True
-    assert b.contains(89) is False
+    assert big_left.contains(20) is True
+
+
+def test_not_contained(big_left):
+    """Test contains returns False when item not in list."""
+    assert big_left.contains(89) is False
 
 
 def test_size_empty():
-    """Test size function works."""
+    """Test size function works on an empty tree."""
     from bst import BST
     c = BST()
     assert c.size() == 0
 
 
 def test_size_one():
-    """Test size function works."""
+    """Test size function works on a single Node Tree."""
     from bst import BST
     d = BST()
     d.insert(20)
     assert d.size() == 1
 
 
-def test_size_many():
+def test_size_many(big_left):
     """Test many and similar insert size."""
-    from bst import BST
-    b = BST([20])
-    b.insert(16)
-    b.insert(14)
-    b.insert(17)
-    b.insert(14)
-    b.insert(23)
-    assert b.size() == 5
+    assert big_left.size() == 5
 
 
 def test_depth_none():
@@ -85,16 +89,9 @@ def test_balance_one():
     assert a.balance() == 0
 
 
-def test_balance_left():
+def test_balance_left(big_left):
     """Test that a left side is larger and returns a positive int."""
-    from bst import BST
-    b = BST([20])
-    b.insert(16)
-    b.insert(14)
-    b.insert(17)
-    b.insert(14)
-    b.insert(23)
-    assert b.balance() > 0
+    assert big_left.balance() > 0
 
 
 def test_balance_right():
@@ -113,12 +110,7 @@ def test_depth_many():
     a.left = Node(16)
     assert a.depth() == 2
 
-def test_depth_tree():
-    from bst import BST
-    b = BST([20])
-    b.insert(16)
-    b.insert(14)
-    b.insert(17)
-    b.insert(14)
-    b.insert(23)
-    assert b.depth() == 3
+
+def test_depth_tree(big_left):
+    """Assert that a tree dpeth is three."""
+    assert big_left.depth() == 3
