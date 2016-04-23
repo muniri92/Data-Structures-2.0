8c5ab98b163397a8b203dcfb25b3944480adf765
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-23 07:58:21 -0700

    tests rewritten so the will pass.

--- src/test_insertion_sort.py
+++ src/test_insertion_sort.py
@@ -9,47 +9,41 @@ RANDOM_LIST = [random.sample(range(1000),
 
 def test_random_lst():
     """A."""
-    from insertion_sort import InsertionSort
-    i = InsertionSort()
+    from insertion_sort import insertion
     for lst in RANDOM_LIST:
         lst_sort = sorted(lst)
-        assert i.insertion(lst) == lst_sort
+        assert insertion(lst) == lst_sort
 
 
 def test_insertion_empty():
     """Test that was can pass an empty list."""
-    from insertion_sort import InsertionSort
-    i = InsertionSort()
-    assert i.insertion([]) == []
+    from insertion_sort import insertion
+    assert insertion([]) == []
 
 
 def test_error_string():
     """Test to see that error is raised when string is passed."""
-    from insertion_sort import InsertionSort
-    i = InsertionSort()
+    from insertion_sort import insertion
     with pytest.raises(TypeError):
-        i.insertion([10, 20, "hey", 20])
+        insertion([10, 20, "hey", 20])
 
 
 def test_error_list():
     """Test to see that error is raised when list is passed."""
-    from insertion_sort import InsertionSort
-    i = InsertionSort()
+    from insertion_sort import insertion
     with pytest.raises(TypeError):
-        i.insertion([10, 20, [], 20])
+        insertion([10, 20, [], 20])
 
 
 def test_error_tuple():
     """Test to see that error is raised when tuple is passed."""
-    from insertion_sort import InsertionSort
-    i = InsertionSort()
+    from insertion_sort import insertion
     with pytest.raises(TypeError):
-        i.insertion([10, 20, ("hey", "fail"), 20])
+        insertion([10, 20, ("hey", "fail"), 20])
 
 
 def test_error_dict():
     """Test to see that error is raised when dict is passed."""
-    from insertion_sort import InsertionSort
-    i = InsertionSort()
+    from insertion_sort import insertion
     with pytest.raises(TypeError):
-        i.insertion([10, 20, {"hey": "fail"}, 20])
+        insertion([10, 20, {"hey": "fail"}, 20])
