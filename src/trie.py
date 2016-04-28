09374446e5d9f744e3fa37ec147ccf919ffcb0d7
Nadia Bahrami <nadia.bahrami@gmail.com>
2016-04-28 10:00:50 -0700

    atucomplete limited to top four items.  Yayyyyy

--- src/trie.py
+++ src/trie.py
@@ -42,14 +42,36 @@ class Trie(object):
                 for more_letters in self.traversal(start[key], word + key):
                     yield more_letters
 
+    def autocomplete(self, token):
+        """Return a list of four contained results of a given token."""
+        current = self.root
+        for char in token:
+            if char in current:
+                current = current[char]
+            else:
+                return []
+        words = self.traversal(current, token)
+        lst = []
+        for word in words:
+            lst.append(word)
+        return lst[:4]
 
 if __name__ == '__main__':
     t = Trie()
     t.insert("cat")
+    t.insert("catty")
+    t.insert("church")
+    t.insert("crutch")
     t.insert("cats")
     t.insert("dog")
     t.insert("at")
     print(t.contains("at"))
+    print(t.autocomplete("c"))
+    print(t.autocomplete("ca"))
+    print(t.autocomplete("cat"))
+    print(t.autocomplete("dogs"))
+    print(t.autocomplete("d"))
+
     for i in t.traversal():
         print(i)
     print(t.traversal(t.root))
